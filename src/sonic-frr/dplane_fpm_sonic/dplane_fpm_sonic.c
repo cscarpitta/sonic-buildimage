923583e0e2be3bc56006ac9db1c28ba6eb30d834
Carmine Scarpitta <cscarpit@cisco.com>
2025-09-03 14:20:39 -0500

    [FPM]: Fix dplane_fpm_sonic for multiple SRv6 nexthops
    Currently, there is a bug in the dplane_fpm_sonic to handle ECMP routes.
    When FRR has an ECMP route with multiple SRv6 nexthops, the
    dplane_fpm_sonic encodes the route with only the first SRv6 nextop in
    the Netlink message, ignoring the other nexthops.
    
    As a result, the Netlink message sent to fpmsyncd leads to the
    installation of the route using only the first SRv6 nexthop, breaking
    the EMCP nature of SRv6 routes.
    
    This pull request fixes the issue in the dplane_fpm_sonic to handle all
    SRv6 nexthops in the route.
    
    Signed-off-by: Carmine Scarpitta <cscarpit@cisco.com>

--- src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
+++ src/sonic-frr/dplane_fpm_sonic/dplane_fpm_sonic.c
@@ -1377,6 +1377,70 @@ static ssize_t netlink_vpn_route_msg_encode(int cmd,
 	return NLMSG_ALIGN(req->n.nlmsg_len);
 }
 
+static bool netlink_srv6_vpn_route_msg_encode_multipath(int cmd, struct zebra_dplane_ctx *ctx,
+							uint8_t *data, size_t datalen,
+							const struct nexthop *nexthop,
+							struct nlmsghdr *nlmsg, size_t req_size,
+							bool fpm, bool force_nhg)
+{
+	struct rtattr *nest;
+	struct rtnexthop *rtnh;
+	struct interface *ifp;
+	struct in6_addr encap_src_addr = {};
+	struct connected *connected;
+	struct vrf *vrf;
+	struct prefix *cp;
+
+	rtnh = nl_attr_rtnh(nlmsg, req_size);
+	if (rtnh == NULL)
+		return false;
+
+	if (!nl_attr_put16(nlmsg, req_size, RTA_ENCAP_TYPE, FPM_ROUTE_ENCAP_SRV6))
+		return false;
+
+	nest = nl_attr_nest(nlmsg, req_size, RTA_ENCAP);
+	if (!nest)
+		return false;
+
+	/*
+     * by default, we use the loopback address as encap source address,
+     * if it is valid
+     */
+	ifp = if_lookup_by_name("lo", VRF_DEFAULT);
+
+	if (ifp) {
+		vrf = vrf_lookup_by_name(VRF_DEFAULT_NAME);
+		if (!vrf)
+			return false;
+
+		FOR_ALL_INTERFACES (vrf, ifp) {
+			frr_each (if_connected, ifp->connected, connected) {
+				cp = connected->address;
+				if (cp->family == AF_INET6 &&
+				    !IN6_IS_ADDR_LOOPBACK(&cp->u.prefix6) &&
+				    !IN6_IS_ADDR_LINKLOCAL(&cp->u.prefix6)) {
+					encap_src_addr = cp->u.prefix6;
+					break;
+				}
+			}
+		}
+	}
+
+	if (!nl_attr_put(nlmsg, req_size, FPM_ROUTE_ENCAP_SRV6_ENCAP_SRC_ADDR, &encap_src_addr,
+			 IPV6_MAX_BYTELEN))
+		return false;
+
+	if (!nl_attr_put(nlmsg, req_size, FPM_ROUTE_ENCAP_SRV6_VPN_SID,
+			 &nexthop->nh_srv6->seg6_segs->seg[0], IPV6_MAX_BYTELEN))
+		return false;
+
+	nl_attr_nest_end(nlmsg, nest);
+
+	nl_attr_rtnh_end(nlmsg, rtnh);
+
+	return true;
+}
+
 /*
  * SRv6 VPN route change via netlink interface, using a dataplane context object
  *
@@ -1400,6 +1464,7 @@ static ssize_t netlink_srv6_vpn_route_msg_encode(int cmd,
 	struct connected *connected;
 	struct vrf *vrf;
 	struct prefix *cp;
+	unsigned int nexthop_num;
 
 	struct {
 		struct nlmsghdr n;
@@ -1474,6 +1539,60 @@ static ssize_t netlink_srv6_vpn_route_msg_encode(int cmd,
 			"%s: %s %pFX vrf %u(%u)", __func__,
 			nl_msg_type_to_str(cmd), p, dplane_ctx_get_vrf(ctx),
 			table_id);
+
+	/*
+	 * Counts the number of nexthops to determine if the route is singlepath
+	 * (single nexthop) or multipath (multiple nexthops)
+	 */
+	nexthop_num = 0;
+	for (ALL_NEXTHOPS_PTR(dplane_ctx_get_ng(ctx), nexthop)) {
+		if (CHECK_FLAG(nexthop->flags, NEXTHOP_FLAG_RECURSIVE))
+			continue;
+		if (!NEXTHOP_IS_ACTIVE(nexthop->flags))
+			continue;
+
+		nexthop_num++;
+	}
+
+	/* Multipath case */
+	if (nexthop_num > 1) {
+		struct rtattr *nest;
+
+		nest = nl_attr_nest(&req->n, datalen, RTA_MULTIPATH);
+		if (nest == NULL)
+			return 0;
+
+		nexthop_num = 0;
+		for (ALL_NEXTHOPS_PTR(dplane_ctx_get_ng(ctx), nexthop)) {
+			if (CHECK_FLAG(nexthop->flags, NEXTHOP_FLAG_RECURSIVE))
+				continue;
+
+			/* Skip non-SRv6 nexthops */
+			if (!nexthop->nh_srv6 || sid_zero(nexthop->nh_srv6->seg6_segs))
+				continue;
+
+			if (NEXTHOP_IS_ACTIVE(nexthop->flags)) {
+				nexthop_num++;
+
+				if (!netlink_srv6_vpn_route_msg_encode_multipath(cmd, ctx, data,
+										 datalen, nexthop,
+										 &req->n, datalen,
+										 fpm, force_nhg))
+					return 0;
+			}
+		}
+
+		nl_attr_nest_end(&req->n, nest);
+
+		if (nexthop_num == 0) {
+			if (IS_ZEBRA_DEBUG_FPM)
+				zlog_debug("%s: No useful nexthop.", __func__);
+		}
+
+		return NLMSG_ALIGN(req->n.nlmsg_len);
+	}
+
+	/* Singlepath case */
 
 	if (!nl_attr_put16(&req->n, datalen, RTA_ENCAP_TYPE,
 				FPM_ROUTE_ENCAP_SRV6))
